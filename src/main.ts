271ddec3289dd96f568a9d73b32e051a41affec4
xaraf <mohamed.ce.ao@gmail.com>
2024-06-26 17:12:05 +0100

    Adding the  ability to apply some filters to the image

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { handleUpload } from "./utils";
+import { applyFilter, handleUpload } from "./utils";
 
 console.log("hello image editor");
-const upload = document.getElementById("upload") as HTMLInputElement;
+
 export const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 export const ctx = canvas.getContext("2d")!;
 
+const upload = document.getElementById("upload") as HTMLInputElement;
+const grayscale = document.getElementById("grayscale") as HTMLButtonElement;
+const sepia = document.getElementById("sepia") as HTMLButtonElement;
+const blur = document.getElementById("blur") as HTMLButtonElement;
+
 upload.addEventListener("change", handleUpload);
+
+grayscale.addEventListener("click", () => applyFilter("grayscale(100%)"));
+sepia.addEventListener("click", () => applyFilter("sepia(100%)"));
+blur.addEventListener("click", () => applyFilter("blur(5px)"));
--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,23 @@
 import { canvas, ctx } from "./main";
 
 export function handleUpload(event: Event) {
-    const files = (event.target as HTMLInputElement).files;
-    if (!files) return console.log("No file is found!?");
-    const file = files[0];
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      const image = new Image();
-      image.src = e.target?.result as string;
-      image.onload = () => {
-        canvas.width = image.width;
-        canvas.height = image.height;
-        ctx.drawImage(image, 0, 0);
-      };
+  const files = (event.target as HTMLInputElement).files;
+  if (!files) return console.log("No file is found!?");
+  const file = files[0];
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    const image = new Image();
+    image.src = e.target?.result as string;
+    image.onload = () => {
+      canvas.width = image.width;
+      canvas.height = image.height;
+      ctx.drawImage(image, 0, 0);
     };
-    reader.readAsDataURL(file);
-  }
+  };
+  reader.readAsDataURL(file);
+}
+
+export function applyFilter(filter: string) {
+  ctx.filter = filter;
+  ctx.drawImage(canvas, 0, 0);
+}
